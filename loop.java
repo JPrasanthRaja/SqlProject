3f182af08b74a3fe879cacd06dc8b89e5c7ba9f2
JPrasanthRaja <prasanthmary1995@gmail.com>
2024-05-20 20:16:06 +0530

    Modified loop.java

--- loop.java
+++ loop.java
@@ -3,8 +3,15 @@ package SqlProject;
 public class loop {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 5; i++) {
-            System.out.println("Hello");
+        // for (int i = 0; i <= 5; i++) {
+        // System.out.println("Hello");
+        // }
+
+        int i = 0;
+
+        while (i <= 5) {
+            System.out.println("Hi");
+            i++;
         }
 
     }
